cdb96551b93427e276581e5dd21e59b36457e7a1
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-09-17 16:39:49 +0300

    finish chat service

--- chat-service/src/main/java/com/project/chatservice/ChatserviceApplication.java
+++ chat-service/src/main/java/com/project/chatservice/ChatserviceApplication.java
@@ -9,4 +9,46 @@ public class ChatserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChatserviceApplication.class, args);
 	}
+
+	@Bean
+	public UnicastProcessor<Event> eventPublisher(){
+		return UnicastProcessor.create();
+	}
+
+	@Bean
+	public Flux<Event> events(UnicastProcessor<Event> eventPublisher) {
+		return eventPublisher
+				.replay(25)
+				.autoConnect();
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> routes(){
+		return RouterFunctions.route(
+				GET("/"),
+				request -> ServerResponse.ok().body(BodyInserters.fromResource(new ClassPathResource("public/index.html")))
+		);
+	}
+
+	@Bean
+	public HandlerMapping webSocketMapping(UnicastProcessor<Event> eventPublisher, Flux<Event> events) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("/websocket/chat", new ChatSocketHandler(eventPublisher, events));
+		SimpleUrlHandlerMapping simpleUrlHandlerMapping = new SimpleUrlHandlerMapping();
+		simpleUrlHandlerMapping.setUrlMap(map);
+
+		//Without the order things break :-/
+		simpleUrlHandlerMapping.setOrder(10);
+		return simpleUrlHandlerMapping;
+	}
+
+	@Bean
+	public WebSocketHandlerAdapter handlerAdapter() {
+		return new WebSocketHandlerAdapter();
+	}
+
+	@Bean
+	public UserStats userStats(Flux<Event> events, UnicastProcessor eventPublisher){
+		return new UserStats(events, eventPublisher);
+	}
 }
